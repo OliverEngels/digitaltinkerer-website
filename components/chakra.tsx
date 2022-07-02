46fdf983cca1755c5cc4c0f4221deacb8b61d5fa
OliverEngels <oliverngels@outlook.com>
2022-07-02 12:43:46 +0200

    edit(light-mode): set background white to off white

--- components/chakra.tsx
+++ components/chakra.tsx
@@ -5,13 +5,23 @@ import {
     localStorageManager,
     type ThemeConfig
 } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
 const config: ThemeConfig = {
     initialColorMode: 'dark',
     useSystemColorMode: false
 }
 
-export const theme = extendTheme({ config })
+export const theme = extendTheme({
+    config,
+    styles: {
+        global: props => ({
+            body: {
+                bg: mode('#FDFDFD', '')(props)
+            }
+        })
+    }
+})
 
 export default function Chakra({ cookies, children }) {
     const colorModeManager =
