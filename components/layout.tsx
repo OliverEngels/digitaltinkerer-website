54ae41bb4a1046cd2a22201af7bfef91a4603b5f
OliverEngels <oliverngels@outlook.com>
2022-07-02 12:13:07 +0200

    fix(seo): adding description

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,6 +13,10 @@ const Layout = ({ children, title }) => (
             <Head>
                 <title>{title} - Oliver Engels</title>
                 <meta property='og:title' content={title} />
+                <meta
+                    name='description'
+                    content='Oliver Engels work experience and tinkering’s, showing his abilities in various professional skills.'
+                />
             </Head>
         )}
         {children}
